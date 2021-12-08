fc9ebb1da90dcf3f31498446d3eca276fb893e17
rakshitha.mendon@arisglobal.com <rakshitha.mendon@arisglobal.com>
2021-12-08 22:06:30 +0530

    m6

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-project';
+  name="Rakshitha";
+  getName(){
+    return this.name;
+  }
+  arr=[1,2,3];
+  person={
+    name:"Tom"
+  }
+}
